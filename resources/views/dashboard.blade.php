97be1dc5973cb007da6280ada7050863d62fbd80
JhongDelosReyes <138560599+JhongDelosReyes@users.noreply.github.com>
2023-07-24 14:15:28 +0800

    initial commit
    initial commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,31 @@
+<x-app-layout >
+    <head>
+        <h6 class="font-semibold text-xl text-black leading-tight bg-blue-500">
+            {{ __('GoveSmart Solution Inc.') }}
+        </h6>
+    </head>
+    
+    <div style="background-image: url('{{ asset('images/space.jpg.png') }}'); background-size: cover; background-position: center;">
+        <img class="max-w-full" src="{{ URL('images/space.jpg.png') }}" alt="">
+        <div class="p-6 text-white">
+            {{ __("You're logged in!") }}
+        </div>
+        
+    
+        
+    
+    <div class="py-12">
+        
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            
+            <div class="bg-black overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="bg-black overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="flex justify-center">
+                        
+                    </div>
+                
+            </div>
+        </div>
+    </div>
+    </div>
+</x-app-layout >
